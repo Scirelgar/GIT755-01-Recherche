0eb9c4b658ff178d0eaffcd1815a8cb378a2dcb0
Félix Wilhelmy <fwilhelmy@hotmail.com>
2024-04-02 18:41:51 -0400

    Cleaning and improving the quantum layers

--- src/layers/QuanvolutionLayer.py
+++ src/layers/QuanvolutionLayer.py
@@ -5,27 +5,24 @@ import torch
 import torch.nn as nn
 import pennylane as qml
 from pennylane import numpy as np
-from pennylane.templates import RandomLayers
 
-class Quanvolution(nn.Module):
-    def __init__(self, kernel_size=(2,2), stride=2, padding=0, n_layers=1):
-        super(Quanvolution, self).__init__()
+class QuanvolutionLayer(nn.Module):
+    def __init__(self, kernel_size=(2,2), stride=2, padding=0, n_qdepth=1):
+        super(QuanvolutionLayer, self).__init__()
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        self.n_layers = n_layers
-
-        self.dev = qml.device("default.qubit", wires=self.kernel_size[0]*kernel_size[1])
-        # Random circuit parameters
+        self.n_qdepth = n_qdepth
         self.circuit = self.qnode()
-
         
-    def qnode(self): 
-        weight_shapes = {"weights": (self.n_layers, self.n_qubits())}
+    def qnode(self):
+        n_qubits = self.kernel_size[0]*self.kernel_size[1]
+        dev = qml.device("default.qubit", wires=n_qubits)
+        weight_shapes = {"weights": (self.n_qdepth, n_qubits)}
 
-        @qml.qnode(self.dev)
+        @qml.qnode(dev)
         def circuit(inputs, weights):
-            for j in range(self.n_qubits()):
+            for j in range(n_qubits):
                 qml.RY(np.pi *inputs[j], wires=j)
 
             qml.RX(weights[0, 0], wires=0)
@@ -33,35 +30,37 @@ class Quanvolution(nn.Module):
             qml.CNOT(wires=[2, 3])
             qml.CNOT(wires=[0, 2])
             qml.CNOT(wires=[0, 3])
-            qml.RY(weights[0, 3], wires=0)
-            qml.RY(weights[0, 4], wires=3)
+            qml.RY(weights[0, 2], wires=0)
+            qml.RY(weights[0, 3], wires=3)
 
             return [qml.expval(qml.PauliZ(j)) for j in range(self.n_qubits())]
-        
         return qml.qnn.TorchLayer(circuit, weight_shapes)
     
-    def n_qubits(self):
-        return self.kernel_size[0]*self.kernel_size[1]
-
     def forward(self, x):
-        # TODO Calculate the actual output size
-        out = np.zeros((x.shape[0], int(x.shape[2]/self.kernel_size[0]), int(x.shape[3]/self.kernel_size[1]), self.n_qubits()))
+        n_qubits = self.kernel_size[0]*self.kernel_size[1]
+        out = np.zeros((
+            x.shape[0], 
+            n_qubits, 
+            int(x.shape[2]/self.kernel_size[0]), 
+            int(x.shape[3]/self.kernel_size[1])))
 
-        # TODO Not sure 100% about the x.shape[0] and x.shape[1] values
+        # Loop over the batch size
         for i in range(x.shape[0]):
+            # Loop over the coordinates of the top-left pixel of 2X2 squares
             for j in range(0, x.shape[2], self.kernel_size[0]):
                 for k in range(0, x.shape[3], self.kernel_size[1]):
                     # Process a region of the image with a quantum circuit
                     q_results = self.circuit(
-                        np.array([
+                        torch.tensor([
                             x[i, 0, j, k],
                             x[i, 0, j, k + 1],
                             x[i, 0, j + 1, k],
                             x[i, 0, j + 1, k + 1]
                         ])
                     )
-                    # Assign expectation values to different channels of the output pixel (j/2, k/2)
-                    for c in range(self.kernel_size[0]*self.kernel_size[1]):
-                        out[i, j // 2, k // 2, c] = q_results[c]
 
-        return out
+                    # Assign expectation values to different channels of the output pixel (j/2, k/2)
+                    for c in range(n_qubits):
+                        out[i, c, j // 2, k // 2] = q_results[c]
+
+        return torch.tensor(out)
---
+++ src/layers/VQCLayer.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import pennylane as qml
+import math
+
+def qnode1(n_qubits, n_qdepth):
+    weight_shapes = {"weights": (n_qdepth, n_qubits, 3)}
+    dev = qml.device("default.qubit", wires=n_qubits)
+    @qml.qnode(dev)
+    def circuit(inputs, weights):
+        qml.AngleEmbedding(inputs, wires=range(n_qubits), rotation='X')
+        qml.StronglyEntanglingLayers(weights, wires=range(n_qubits))
+        return [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
+    return qml.qnn.TorchLayer(circuit, weight_shapes)
+
+def qnode2(n_qubits, n_qdepth):
+    weight_shapes = {"weights": (n_qdepth, n_qubits)}
+    dev = qml.device("default.qubit", wires=n_qubits)
+    @qml.qnode(dev)
+    def circuit(inputs, weights):
+        qml.AngleEmbedding(inputs, wires=range(n_qubits), rotation='X')
+        qml.BasicEntanglerLayers(weights, wires=range(n_qubits))
+        return [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
+    return qml.qnn.TorchLayer(circuit, weight_shapes)
+    
+class VQCLayer(nn.Module):
+    def __init__(self, input, n_qubits, n_qdepth):
+        super(VQCLayer, self).__init__()
+        self.input = input
+        self.n_qubits = n_qubits
+        self.n_qdepth = n_qdepth
+
+        self.layers = []        
+        for _ in range(math.ceil(input/n_qubits)):
+            # Generate a quantum node for
+            self.layers.append(qnode1(n_qubits, n_qdepth))
+
+    def forward(self, x):
+        # Split the input into the number of qubits
+        x_split = torch.split(x, self.n_qubits, dim=1)
+        outputs = []
+        for layer, inputs in zip(self.layers, x_split):
+            outputs.append(layer(inputs))
+        
+        # Concatenate the outputs of individual quantum layers
+        return torch.cat(outputs, dim=1)
