db2e9f9271bcbc78609400d5684788d2288832b9
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2024-04-04 16:30:17 -0400

    Add timestamps while training

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@
 
 # Set the matplotlib backend so figures can be saved in the background
 import matplotlib
+
 matplotlib.use("Agg")
 
 # Import the necessary packages
@@ -19,7 +20,7 @@ import torch
 import time
 from datetime import datetime
 import os
-import pickle 
+import pickle
 
 from models.LeNet import LeNet
 from models.HQNN_Quanv import HQNN_Quanv
@@ -65,7 +66,7 @@ print("[INIT] Initializing the model...")
 # Configure the device we will be using to train the model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Initialize the model
-#model = LeNet(numChannels=1, classes=len(trainData.dataset.classes)).to(device)
+# model = LeNet(numChannels=1, classes=len(trainData.dataset.classes)).to(device)
 model = HQNN_Quanv(in_channels=1, classes=len(trainData.dataset.classes)).to(device)
 # Initialize the optimizer and loss function
 opt = Adam(model.parameters(), lr=INIT_LR)
@@ -76,84 +77,95 @@ H = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
 
 print("[TRAIN] Training the model...")
 
-startTime = time.time() # To measure how long training is going to take
+startTime = time.time()  # To measure how long training is going to take
 
 # loop over our epochs
 for e in range(0, EPOCHS):
-	# Set the model in training mode
-	model.train()
-	# Initialize the total training/validation loss/correct predictions
-	totalTrainLoss, totalValLoss, trainCorrect, valCorrect = 0, 0, 0, 0
-	
-	# Training
-	for (x, y) in trainDataLoader:
-		# Send the input to the device
-		(x, y) = (x.to(device), y.to(device))
-		# Perform a forward pass and calculate the training loss
-		pred = model(x)
-		loss = lossFn(pred, y)
-		# Zero out the gradients, perform the backpropagation step, and update the weights
-		opt.zero_grad()
-		loss.backward()
-		opt.step()
-		# Sum the loss to the total training loss so far
-		totalTrainLoss += loss
-		# Calculate the number of correct predictions
-		trainCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
-	
-	# Validation
-	with torch.no_grad():
-		# Set the model in evaluation mode
-		model.eval()
-		# Loop over the validation set
-		for (x, y) in valDataLoader:
-			# Send the input to the device
-			(x, y) = (x.to(device), y.to(device))
-			# Make the predictions and calculate the validation loss
-			pred = model(x)
-			totalValLoss += lossFn(pred, y)
-			# Calculate the number of correct predictions
-			valCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
+    # Set the model in training mode
+    model.train()
+    # Initialize the total training/validation loss/correct predictions
+    totalTrainLoss, totalValLoss, trainCorrect, valCorrect = 0, 0, 0, 0
 
-	# Calculate the average training and validation loss
-	avgTrainLoss = totalTrainLoss / trainSteps
-	avgValLoss = totalValLoss / valSteps
-	# Calculate the training and validation accuracy
-	trainCorrect = trainCorrect / len(trainDataLoader.dataset)
-	valCorrect = valCorrect / len(valDataLoader.dataset)
+    # Training
+    for x, y in trainDataLoader:
+        # Send the input to the device
+        (x, y) = (x.to(device), y.to(device))
+        # Perform a forward pass and calculate the training loss
+        pred = model(x)
+        loss = lossFn(pred, y)
+        # Zero out the gradients, perform the backpropagation step, and update the weights
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        # Sum the loss to the total training loss so far
+        totalTrainLoss += loss
+        # Calculate the number of correct predictions
+        trainCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
 
-	# Update our training history
-	H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
-	H["train_acc"].append(trainCorrect)
-	H["val_loss"].append(avgValLoss.cpu().detach().numpy())
-	H["val_acc"].append(valCorrect)
-	
-	# Print the model training and validation information
-	print(
-            "[TRAIN] EPOCH: {}/{} | Train loss: {:0.6f} | Train acc: {:0.4f} | Val loss: {:0.6f} | Val acc: {:0.4f}"
-            "".format(e + 1, EPOCHS, avgTrainLoss, trainCorrect, avgValLoss, valCorrect)
+    # Validation
+    with torch.no_grad():
+        # Set the model in evaluation mode
+        model.eval()
+        # Loop over the validation set
+        for x, y in valDataLoader:
+            # Send the input to the device
+            (x, y) = (x.to(device), y.to(device))
+            # Make the predictions and calculate the validation loss
+            pred = model(x)
+            totalValLoss += lossFn(pred, y)
+            # Calculate the number of correct predictions
+            valCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    # Calculate the average training and validation loss
+    avgTrainLoss = totalTrainLoss / trainSteps
+    avgValLoss = totalValLoss / valSteps
+    # Calculate the training and validation accuracy
+    trainCorrect = trainCorrect / len(trainDataLoader.dataset)
+    valCorrect = valCorrect / len(valDataLoader.dataset)
+
+    # Update our training history
+    H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
+    H["train_acc"].append(trainCorrect)
+    H["val_loss"].append(avgValLoss.cpu().detach().numpy())
+    H["val_acc"].append(valCorrect)
+    intervalTime = time.time() - startTime
+
+    # Print the model training and validation information
+    print(
+        "[TRAIN] EPOCH: {}/{} | Train loss: {:0.6f} | Train acc: {:0.4f} | Val loss: {:0.6f} | Val acc: {:0.4f} "
+        "| Time elapsed: {:0.2f} ".format(
+            e + 1,
+            EPOCHS,
+            avgTrainLoss,
+            trainCorrect,
+            avgValLoss,
+            valCorrect,
+            intervalTime,
         )
-	
+    )
+
 # Finish measuring how long training took
 endTime = time.time()
 print("[TRAIN] Finished training the model...")
-print("[TRAIN] Total time taken to train the model: {:.2f}s".format(endTime - startTime))
+print(
+    "[TRAIN] Total time taken to train the model: {:.2f}s".format(endTime - startTime)
+)
 
 print("[END] Evaluating the model...")
 
 ys = []
 # We can now evaluate the network on the test set
-with torch.no_grad(): # Turn off autograd for testing evaluation
-	model.eval() # Set the model in evaluation mode
-	preds = [] # Initialize a list to store our predictions
-	# Evaluation with test dataset
-	for (x, y) in testDataLoader:
-		ys = y
-		# Send the input to the device
-		x = x.to(device)
-		# Make the predictions and add them to the list
-		pred = model(x)
-		preds.extend(pred.argmax(axis=1).cpu().numpy())
+with torch.no_grad():  # Turn off autograd for testing evaluation
+    model.eval()  # Set the model in evaluation mode
+    preds = []  # Initialize a list to store our predictions
+    # Evaluation with test dataset
+    for x, y in testDataLoader:
+        ys = y
+        # Send the input to the device
+        x = x.to(device)
+        # Make the predictions and add them to the list
+        pred = model(x)
+        preds.extend(pred.argmax(axis=1).cpu().numpy())
 
 print("[END] Generating the results...")
 
@@ -184,7 +196,7 @@ os.mkdir(path)
 # Saving the results
 plt.savefig(path + "/plot.png")
 
-with open(path + "/data.pkl", 'wb') as f:
+with open(path + "/data.pkl", "wb") as f:
     pickle.dump(H, f)
 
 # TODO NOT WORKING AT THE MOMENT
